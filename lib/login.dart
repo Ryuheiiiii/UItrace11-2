d7c21be69ba58576558a47a2141f443661cb2a2c
Ryuheiiiii <86302005+Ryuheiiiii@users.noreply.github.com>
2021-11-20 03:47:11 +0900

    UItrace11-2

--- lib/login.dart
+++ lib/login.dart
@@ -10,10 +10,12 @@ class MyhomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-        body: Padding(
-        padding: EdgeInsets.only(top: 80),
-        child: ChangeForm(),
-    )));
+            body: Padding(
+              padding: EdgeInsets.only(top: 80),
+              child: ChangeForm(),
+            )
+        )
+    );
   }
 }
 
@@ -32,87 +34,96 @@ void _handleCheckbox(bool e) {
   });
 }
 
-  Widget build(BuildContext context) {
-    return Column(
-            children: [
-              const SizedBox(height: 50,),
-              Text('Enter your name',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            )),
+Widget build(BuildContext context) {
+  return Column(
+    children: [
+      const SizedBox(height: 50,),
+      Text('Enter your name',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          )),
       const SizedBox(height: 30,),
       Container(
-          child: Row(
+        child: Row(
             children: [
               Container(
-                  padding: EdgeInsets.only(
-                      left: 20
-                  )),
+                padding: EdgeInsets.only(
+                    left: 20
+                ),
+              ),
               Flexible(
-              child: TextField(),),
+                child: TextField(),),
               Container(
                 padding: EdgeInsets.only(
-                  left: 20
-                )),
+                    left: 20
+                ),
+              ),
               Flexible(child: TextField(),),
               Container(
-                  padding: EdgeInsets.only(
-                      right: 20
-                  )),
+                padding: EdgeInsets.only(
+                    right: 20
+                ),
+              ),
             ]
-          )),
+        ),
+      ),
+      Container(
+        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('First'),
             Container(
-                child: Row(mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('First'),
-    Container(
-    padding: EdgeInsets.only(
-    left: 155)),
-                Text('Last')
-                  ])),
+                padding: EdgeInsets.only(
+                    left: 155)),
+            Text('Last')
+          ],
+        ),
+      ),
       SizedBox(height: 5,),
       Container(
         child: Row(mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Checkbox(
-                activeColor: Colors.grey,
-          value: _flag,
-          onChanged: null,
+              activeColor: Colors.grey,
+              value: _flag,
+              onChanged: null,
             ),
-          Text('I am 13 years or older',
-          style: TextStyle(
-            color: Colors.grey,
-          ),)
+            Text('I am 13 years or older',
+                style: TextStyle(
+                  color: Colors.grey,
+                )),
           ],
-        )
+        ),
       ),
-            SizedBox(height: 90,),
-            SizedBox(
-              width: 300,
-                child: ElevatedButton(
-                    onPressed: (){},
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0)),
-                    primary: Colors.grey,
-                      elevation: 0.0,
-            ),
-    child: Text('Sync contacts and continue',
-    style: TextStyle(
-    color: Colors.white
-    ))),),
-            SizedBox(height: 5,),
-            Container(
-              child: TextButton(
-                onPressed: (){},
-                child: Text('Conntinue without syncing contacts',
-                  style: TextStyle(
-                    color: Colors.grey,
-
-                  ),
-                ),
-              )
-            )]);
-    }}
+      SizedBox(height: 90,),
+      SizedBox(
+        width: 300,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            primary: Colors.grey,
+            elevation: 0.0,
+          ),
+          child: Text('Sync contacts and continue',
+              style: TextStyle(
+                  color: Colors.white
+              )),
+        ),
+      ),
+      SizedBox(height: 5,),
+      Container(
+        child: TextButton(
+          onPressed: () {},
+          child: Text('Conntinue without syncing contacts',
+              style: TextStyle(
+                color: Colors.grey,
+              )),
+        ),
+      ),
+    ],
+  );
+}
+}
