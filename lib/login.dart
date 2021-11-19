99d5e46a80998338e437860fb8c3fea4fc1e8f2d
Ryuheiiiii <86302005+Ryuheiiiii@users.noreply.github.com>
2021-11-20 03:28:45 +0900

    UItrace11-2

---
+++ lib/login.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyhomePage());
+}
+
+class MyhomePage extends StatelessWidget {
+  const MyhomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+        body: Padding(
+        padding: EdgeInsets.only(top: 80),
+        child: ChangeForm(),
+    )));
+  }
+}
+
+class ChangeForm extends StatefulWidget {
+  @override
+  _ChangeFormState createState() => _ChangeFormState();
+}
+
+class _ChangeFormState extends State<ChangeForm> {
+
+bool _flag = false;
+
+void _handleCheckbox(bool e) {
+  setState(() {
+    _flag = e;
+  });
+}
+
+  Widget build(BuildContext context) {
+    return Column(
+            children: [
+              const SizedBox(height: 50,),
+              Text('Enter your name',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            )),
+      const SizedBox(height: 30,),
+      Container(
+          child: Row(
+            children: [
+              Container(
+                  padding: EdgeInsets.only(
+                      left: 20
+                  )),
+              Flexible(
+              child: TextField(),),
+              Container(
+                padding: EdgeInsets.only(
+                  left: 20
+                )),
+              Flexible(child: TextField(),),
+              Container(
+                  padding: EdgeInsets.only(
+                      right: 20
+                  )),
+            ]
+          )),
+            Container(
+                child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('First'),
+    Container(
+    padding: EdgeInsets.only(
+    left: 155)),
+                Text('Last')
+                  ])),
+      SizedBox(height: 5,),
+      Container(
+        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Checkbox(
+                activeColor: Colors.grey,
+          value: _flag,
+          onChanged: null,
+            ),
+          Text('I am 13 years or older',
+          style: TextStyle(
+            color: Colors.grey,
+          ),)
+          ],
+        )
+      ),
+            SizedBox(height: 90,),
+            SizedBox(
+              width: 300,
+                child: ElevatedButton(
+                    onPressed: (){},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)),
+                    primary: Colors.grey,
+                      elevation: 0.0,
+            ),
+    child: Text('Sync contacts and continue',
+    style: TextStyle(
+    color: Colors.white
+    ))),),
+            SizedBox(height: 5,),
+            Container(
+              child: TextButton(
+                onPressed: (){},
+                child: Text('Conntinue without syncing contacts',
+                  style: TextStyle(
+                    color: Colors.grey,
+
+                  ),
+                ),
+              )
+            )]);
+    }}
---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:untitled7/login.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Home()
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+          children: [
+            const SizedBox(height: 80,),
+            SizedBox(
+              height: 80,
+                width: 80,
+                child: Image.asset('images/UItrace11-2.png')
+            ),
+            const SizedBox(height: 30,),
+            RichText(
+                text: TextSpan(
+                    style: TextStyle(color: Colors.black),
+    children: [TextSpan(text: '  Sign up to spend more time with your \nfriends ',
+      style: TextStyle(
+          color:Colors.black45,
+        height: 1.5,
+        fontWeight: FontWeight.bold,
+      ),),
+      TextSpan(
+      text: 'IRL',
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+      )),
+    TextSpan(text: ' and deiscover fun things to do \n                               together.',
+    style: TextStyle(
+      color:Colors.black45,
+      height: 1.5,
+      fontWeight: FontWeight.bold,
+    ))
+               ],
+            ),),
+            const SizedBox(height: 50,),
+            SizedBox(
+              height: 50,
+              width: 260,
+              child: SignInButton(
+              Buttons.Google,
+              text: "Continue with Google",
+              onPressed: () {},
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)
+              ),
+              elevation: 0.0,
+            ),),
+            const SizedBox(height: 20,),
+           SizedBox(
+             height: 50,
+               width: 260,
+               child: SignInButtonBuilder(
+              text: 'Sign up with email or phone',
+              icon: Icons.email,
+              onPressed: () {},
+              backgroundColor: Color(0xFFefefef),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)
+              ),
+              textColor: Colors.grey,
+              iconColor: Colors.grey,
+              elevation: 0.0,
+            ),),
+            const SizedBox(height: 7,),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Already have an account?',
+                  style: TextStyle(
+                    color: Colors.grey
+                  ),),
+                  TextButton(
+                    onPressed: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MyhomePage(),),
+                      );
+                    },
+                    child: Text('Log in',
+                    style: TextStyle(
+                      color: Colors.blue
+    ),),
+    )
+    ]
+    )]));
+  }
+}
